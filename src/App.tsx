bab8cadba633a3cefcea67727a30c9da18f44676
hi6724 <hi6724@gmail.com>
2022-06-16 23:37:10 +0900

    초기셋팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,31 @@
+import styled from "styled-components";
+
 function App() {
-  return <>hello world</>;
+  return (
+    <>
+      <Wrapper>
+        <h1>Hello World</h1>
+      </Wrapper>
+    </>
+  );
 }
 
 export default App;
+
+const Wrapper = styled.div`
+  position: relative;
+  background-color: #1f1144;
+  .canvas {
+    width: 100%;
+    height: 100%;
+  }
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  h1 {
+    position: absolute;
+    top: 0;
+  }
+`;
